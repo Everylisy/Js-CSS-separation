e3b6ebd0bbc9286461392ca0891159aa5c50a39d
violetwhenisawu <60170829+violetwhenisawu@users.noreply.github.com>
2022-01-26 00:42:43 +0900

    toggle / className / classList description update
    조금 더 자세한 설명 추가, 코드 간략화

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const title = document.querySelector("h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if(title.classList.contains(clickedClass)){
-        title.classList.remove(clickedClass);    
-    }
-    else {
-        title.classList.add(clickedClass);
-    }
+    // const clickedClass = "clicked";
+    // if(title.classList.contains(clickedClass)){
+    //     title.classList.remove(clickedClass);    
+    // }
+    // else {
+    //     title.classList.add(clickedClass);
+    // }
+
+    /*토글은 toggle은 토큰이 존재하면 토큰제거,
+    토큰이 존재 하지않으면 토큰 추가 (위의 코드와 같은 역할을 하고 있다)*/
+    title.classList.toggle("clicked");      // 토글 = "신" supercoolbeautifulcode!
 }
 
 title.addEventListener("click", handleTitleClick);
